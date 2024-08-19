3cd5b0fcd766e1d9c2eb0f2a9b535862ebc2a0ed
Jörg Schönberger <office@webchills.at>
2024-08-19 12:27:55 +0200

    instant search files added

---
+++ NEUE DATEIEN/includes/languages/polish/instant_search_result.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package  Instant Search Plugin for Zen Cart German
+ * @author   marco-pm
+ * @version  4.0.3
+ * @see      https://github.com/marco-pm/zencart_instantsearch
+ * @license  GNU Public License V2.0
+ * modified for Zen Cart German
+ * 2024-08-19 webchills
+ */
+
+define('NAVBAR_TITLE', 'Wyniki wyszukiwania');
+define('HEADING_TITLE', 'Wyniki wyszukiwania');
+define('TEXT_NO_PRODUCTS_FOUND', 'Nie znaleziono artykułów');
+define('TEXT_LOADING_RESULTS', 'Załaduj wyniki...');
+define('TEXT_SHOW', 'Filtruj wyniki według:');
